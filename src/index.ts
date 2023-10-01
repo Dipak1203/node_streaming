d56c158e2f6f2cdfb51ac9eacda0c5aede15effe
DipakDai <dipakkumal673@gmail.com>
2023-10-01 16:54:32 +0545

    streamming added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import http from 'http';
+import fs from 'fs'
 const server = http.createServer((req, res) => {
-    return res.end("Hello");
+    // const readFile = fs.readFileSync("src/files/sample.txt","utf-8")
+    const readFile = fs.createReadStream("src/files/sample.txt")
+    res.writeHead(200,{"Content-Type": 'text'})
+    readFile.pipe(res);
+    // return res.end(readFile)
 });
 
 const PORT = 3000;
